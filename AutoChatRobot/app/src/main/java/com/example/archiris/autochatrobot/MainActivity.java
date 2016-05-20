37e43a4991519f882ebc275631e24c09feee8ca9
Faldict <legadoubledyan@gmail.com>
2016-05-20 17:53:15 +0800

    layout
    …

--- AutoChatRobot/app/src/main/java/com/example/archiris/autochatrobot/MainActivity.java
+++ AutoChatRobot/app/src/main/java/com/example/archiris/autochatrobot/MainActivity.java
@@ -80,8 +80,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         mStatus = (TextView) findViewById(R.id.tvstatus);
 
